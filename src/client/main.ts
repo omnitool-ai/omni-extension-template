afa1fca358a62cf1dbc4ec64d0ce050f0df12a78
Minhye Kim <popenaga@gmail.com>
2024-02-12 23:08:52 +0800

    Fix errors

--- src/client/main.ts
+++ src/client/main.ts
@@ -2,4 +2,4 @@
 import {OmniSDKClient} from 'omni-sdk';
 const sdk = new OmniSDKClient("<replace with your extension id>").init();
 
-sdk.sendMessageToSession("Hello from Omni Extension!");
+sdk.sendChatMessage("Hello from Omni Extension!");
